a1e08563475ff51e976ee8974107f46010638137
Luis Tellez <luistellez@gmail.com>
2013-12-19 21:07:01 -0200

    Fixing Insert and Delete
    Fixing Insert and Delete issues after the Generic Factory.

--- Classifieds.Domain/UOW/GenericRepository.cs
+++ Classifieds.Domain/UOW/GenericRepository.cs
@@ -56,6 +56,7 @@ namespace Classifieds.Domain.UOW
         public virtual void Insert(TEntity entity)
         {
             dbSet.Add(entity);
+            context.SaveChanges();
         }
 
         public virtual void Delete(object id)
@@ -70,13 +71,16 @@ namespace Classifieds.Domain.UOW
             {
                 dbSet.Attach(entityToDelete);
             }
+
             dbSet.Remove(entityToDelete);
+            context.SaveChanges();
         }
 
         public virtual void Update(TEntity entityToUpdate)
         {
             dbSet.Attach(entityToUpdate);
             context.Entry(entityToUpdate).State = EntityState.Modified;
+            context.SaveChanges();
         }
     }
 }
